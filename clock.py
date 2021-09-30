bc9ef11c700593b53680fc82f42c2b6c5b494ca6
Jesse Phillips <JamesPhillipsUK@users.noreply.github.com>
2021-09-30 23:41:04 +0100

    Added documentation and a theme.  Tweaked some UI elements and logic.

--- clock.py
+++ clock.py
@@ -1,33 +1,58 @@
-import PySimpleGUI as psg
-import datetime
+"""
+clock.py
+A simple Python-powered clock for my Raspberry Pi.  Using PySimpleGUI to make it look nice.
 
-def launchGameLoop(window):
+Authors
+-------
+  Jesse Phillips <james@jamesphillipsuk.com>
+"""
 
+import PySimpleGUI as psg # PySimpleGUI UI Library
+import datetime # It's a clock.
+
+def launchEventLoop(window):
+  """
+  This method contains the event loop for the application.  It takes the applications window as a parameter.
+  
+  Parameters
+  ----------
+  window: window
+    The application window.
+  """
+  # BEGIN THE EVENT LOOP
   while True:
-    event, values = window.read(timeout = 1000)
+    event, values = window.read(timeout = 1000) # Only need to update once per second.
     if event == psg.WIN_CLOSED:
-      break
-    window['time'].update(datetime.datetime.now().strftime('%H:%M:%S'))
-    if datetime.datetime.now().time() < datetime.time(12) and datetime.datetime.now().time() >= datetime.time(6):
+      break # End the event runtime if the user requests to close the window.
+    
+    window['time'].update(datetime.datetime.now().strftime('%H:%M:%S')) # Update the clock.
+
+    # Display a nice message to the user.
+    now = datetime.datetime.now().time()
+    if now < datetime.time(12) and now >= datetime.time(6):
       window['greeting'].update("Good morning")
-    elif datetime.datetime.now().time() >= datetime.time(12) and datetime.datetime.now().time() < datetime.time(18):
+    elif now >= datetime.time(12) and now < datetime.time(18):
       window['greeting'].update("Good afternoon")
-    elif datetime.datetime.now().time() >= datetime.time(18) and datetime.datetime.now().time() < datetime.time(22):
+    elif now >= datetime.time(18) and now < datetime.time(22):
       window['greeting'].update("Good evening")
     else:
       window['greeting'].update("Good night")
+  # KILL THE WINDOW ONCE THE EVENT RUNTIME IS OVER.
   window.close()
 
 def main():
-  layout = [
-    [psg.Text("The time is:", font = ('Ubuntu', 100), justification = 'center')],
-    [psg.Text("00:00:00", size = (8, 1), font = ('Ubuntu', 100), justification = 'center', key = 'time')],
-    [psg.Text("Good morning", size = (15, 1), font = ('Ubuntu', 100), justification = 'center', key = 'greeting')]
+  """
+  Main method for the application.
+  """
+  psg.theme('DarkAmber') # Set a cool-looking theme, easy on the eyes.
+  layout = [ # The Window layout.  Filled with default values.
+    [psg.Text("The time is:", font = 'Ubuntu 100', justification = 'center')],
+    [psg.Text("00:00:00", size = (8, 1), font = 'Ubuntu 165 bold', justification = 'center', key = 'time')],
+    [psg.Text("Good morning", size = (15, 1), font = 'Ubuntu 96', justification = 'center', key = 'greeting')]
   ]
-  window = psg.Window(title="Clock", layout=layout, size=(1024, 600), element_justification='c', finalize = True)
+  window = psg.Window(title="Clock", layout=layout, size=(1024, 600), element_justification='c', finalize = True) # Build the application window.
   window.Maximize()
-  launchGameLoop(window)
-  print("ran successfully!")
+  launchEventLoop(window) # Run the event loop for the application on the window.
   
 if __name__ == "__main__":
   main()
